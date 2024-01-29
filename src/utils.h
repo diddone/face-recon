9e9425027c488d3c525b2eb05dc0ead0a99fb25e
biraysutcuoglu <birays1999@gmail.com>
2024-01-29 18:20:57 +0100

    fix vertex normals function

--- src/utils.h
+++ src/utils.h
@@ -257,32 +257,27 @@ public:
             Eigen::Vector3d vertB = face_vertices[index2];
             Eigen::Vector3d vertC = face_vertices[index3];
 
-            Eigen::Vector3d vertNormalA = computeVNormal(vertA, vertB, vertC);
-            
-            //if triangle is lying on single plane normal of every vertex of a triangle is the same
-            Eigen::Vector3d vertNormalB = vertNormalA;
-            Eigen::Vector3d vertNormalC = vertNormalA;
 
-            vertexNormals.push_back(vertNormalA);
-            vertexNormals.push_back(vertNormalB);
-            vertexNormals.push_back(vertNormalC);
+            Eigen::Vector3d faceNormal = computeVNormal(vertA, vertB, vertC);
+            //if triangle is lying on single plane normal of every vertex of a triangle is the same
+            vertexNormals.push_back(faceNormal);
+            vertexNormals.push_back(faceNormal);
+            vertexNormals.push_back(faceNormal);
+        }
+        //normalize every vertex normal
+        for(size_t i = 0; i < vertexNormals.size(); i++){
+            vertexNormals[i].normalize();
         }
         return vertexNormals;
     }
     
     //computes normal of the given vertex 
     Eigen::Vector3d computeVNormal(const Eigen::Vector3d& a, const Eigen::Vector3d& b, const Eigen::Vector3d& c){
-        Eigen::Vector3d sum_vec(0.0f, 0.0f, 0.0f);
+        Eigen::Vector3d norm_vec(0.0f, 0.0f, 0.0f);
         Eigen::Vector3d AB = b - a; 
         Eigen::Vector3d AC = c - a; 
 
-        sum_vec += AB.cross(AC);
-
-        if(sum_vec.norm() == 0){
-            return sum_vec;
-        }
-
-        return sum_vec.normalized();
+        return AB.cross(AC);
     }
 
 
